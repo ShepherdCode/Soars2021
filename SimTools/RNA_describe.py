6671200567bb775834fc94328c6c41fab59d362a
JasonRafeMiller <jmill02@shepherd.edu>
2021-05-13 14:45:37 -0400

    Test get_longest_orf().

--- SimTools/RNA_describe.py
+++ SimTools/RNA_describe.py
@@ -1,32 +1,28 @@
 class RNA_describer():
     START='ATG'
     STOPS=['TAA','TAG','TGA']
-    CLEN=3 # codon length
+    CODON_LEN=3 # codon length
     def get_orf_length(self,seq):
-        '''Returns non-zero only if
-        given sequence begins START
-        and contains a STOP at offset 3n.'''
-        clen=RNA_describer.CLEN
-        if len(seq)<2*clen:
-            return 0 # too short
+        '''Count bases to end of first in-frame stop codon.
+        Return count start and stop codons (min len 6).
+        Assume first three bases are ATG.
+        Assume we'll find a STOP codon at some 3n position.
+        Else return zero.'''
+        clen=RNA_describer.CODON_LEN
+        if len(seq)<clen+clen:
+            return 0 # too short for start + stop
         if not seq[0:clen] == RNA_describer.START:
             return 0 # missing start
         for pos in range(0,len(seq)-clen+1,clen):
             if seq[pos:pos+clen] in RNA_describer.STOPS:
                 return pos+clen
         return 0 # missing stop
-
-    def get_longest_orf_len(self,seq):
+    def get_longest_orf(self,seq):
+        clen=RNA_describer.CODON_LEN
         rlen = len(seq)
-        lens=[0]*RNA_describer.CLEN
-        for frame in range(0,RNA_describer.CLEN):
-            start=0
-        return 0
-
-rn = RNA_describer()
-print("orf len 0, no start:",rn.get_orf_length('TGAAATGA'))
-print("orf len 6, start+stop:",rn.get_orf_length('ATGTGA'))
-print("orf len 9, one codon:",rn.get_orf_length('ATGAAATGA'))
-print("orf len 9, 3'UTR:",rn.get_orf_length('ATGAAATGAAAA'))
-print("orf len 0:, no stop",rn.get_orf_length('ATGAATGA'))
-#print(rn.get_longest_orf_len('ATGAAATGA'))
+        longest_orf=(0,0) # offset, len
+        for one_pos in range(0,rlen):
+            one_len = self.get_orf_length(seq[one_pos:])
+            if one_len > longest_orf[1]:
+                longest_orf=(one_pos,one_len)
+        return longest_orf
--- SimTools/test_RNA_describe.py
+++ SimTools/test_RNA_describe.py
@@ -4,15 +4,25 @@ from RNA_describe import RNA_describer
 class Test_RNA_describer():
     def test_orf_length(self):
         rn = RNA_describer()
-        # Require sequence starts with ATG
-        assert(rn.get_orf_length('TGATGTGA')==0)
-        # Minimum requirement is start and stop
-        assert(rn.get_orf_length('ATG'+'TGA')==6)
-        # Start + codon + stop = 3*3=9
-        assert(rn.get_orf_length('ATG'+'AAA'+'TGA')==9)
-        # polyA tail or any 3'UTR does not count
-        assert(rn.get_orf_length('ATG'+'AAA'+'TGA'+'AAAA')==9)
-        # No in-frame stop? Then no ORF
-        assert(rn.get_orf_length('ATG'+'AA'+'TGA')==0)
+        msg= "Require sequence starts with ATG"
+        assert rn.get_orf_length('TGATGTGA')==0,msg
+        msg = "Minimum requirement is start and stop"
+        assert rn.get_orf_length('ATG'+'TGA')==6,msg
+        msg = "Start + codon + stop = 3*3=9"
+        assert rn.get_orf_length('ATG'+'AAA'+'TGA')==9,msg
+        msg = "polyA tail or any 3'UTR does not count"
+        assert rn.get_orf_length('ATG'+'AAA'+'TGA'+'AAAA')==9,msg
+        msg = "No in-frame stop? Then no ORF."
+        assert rn.get_orf_length('ATG'+'AA'+'TGA')==0,msg
+    def test_longest_orf(self):
+        rn = RNA_describer()
+        msg = "Counts bases ATG thru TAA in frame 0"
+        assert rn.get_longest_orf('ATG'+'TAA'+'G')==(0,6),msg
+        msg = "Counts bases ATG thru TAA in frame 1"
+        assert rn.get_longest_orf('CCC'+'ATG'+'AAA'+'TAA')==(3,9),msg
+        msg = "Counts bases ATG thru TAG in frame 2"
+        assert rn.get_longest_orf('CC'+'ATG'+'AAA'+'TAG')==(2,9),msg
+        msg = "Counts bases ATG thru TGA in frame 3"
+        assert rn.get_longest_orf('C'+'ATG'+'AAA'+'TGA')==(1,9),msg
 
 #assert(rn.get_longest_orf_len('ATGAAATGA'))
