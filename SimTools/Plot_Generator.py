aaf751747835926c5bcae96c29ee7d53cb6697a0
a-moseman <drewmoseman@gmail.com>
2021-06-07 14:40:17 -0400

    Add Plot_Generator.

---
+++ SimTools/Plot_Generator.py
@@ -0,0 +1,170 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+class Plot_Generator:
+	"""
+	Class for generating plots.
+	"""
+	def __init__(self):
+		"""
+		Initialize self.
+		Sets all settables to their default values.
+		"""
+		self.x_scale = None #matplotlib defaults to linear if not specified
+		self.x_base = 10
+		self.y_scale = None #matplotlib defaults to linear if not specified
+		self.y_base = 10
+		self.x_tick_label_rotation = 0
+		self.x_tick_label_horizontal_alignment = 'center'
+		self.y_tick_label_rotation = 0
+		self.y_tick_label_horizontal_alignment = 'center'
+
+		self.title = ""
+		self.x_label = ""
+		self.y_label = ""
+		self.x_tick_labels = None
+		self.y_tick_labels = None
+
+	def set_text(self, title, x_label, y_label, x_tick_labels, y_tick_labels):
+		"""Sets the text of plots.
+		"""
+		self.title = title
+		self.x_label = x_label
+		self.y_label = y_label
+		self.x_tick_labels = x_tick_labels
+		self.y_tick_labels = y_tick_labels
+
+	def set_text_options(self, x_tick_label_rotation, x_tick_label_horizontal_alignment, y_tick_label_rotation, y_tick_label_horizontal_alignment):
+		"""
+		Sets the text options of plots.
+		"""
+		self.x_tick_label_rotation = x_tick_label_rotation
+		self.x_tick_label_horizontal_alignment = x_tick_label_horizontal_alignment
+		self.y_tick_label_rotation = y_tick_label_rotation
+		self.y_tick_label_horizontal_alignment = y_tick_label_horizontal_alignment
+
+	def set_axis_options(self, x_scale, x_base, y_scale, y_base):
+		"""
+		Set the axis options of the plots.
+		Changing the bases does nothing if the scale is None or 'linear'.
+		"""
+		if x_scale == 'linear':
+			self.x_scale = None
+		else:
+			self.x_scale = x_scale
+		self.x_base = x_base
+		if y_scale == 'linear':
+			self.y_scale = None
+		else:
+			self.y_scale = y_scale
+		self.y_base = y_base
+
+	def comparision_bar_plot(self, data_a, data_b, data_a_name, data_b_name):
+		"""
+		Generates a bar plot comparing two data sets.
+		"""
+		assert len(data_a) == len(data_b)
+
+		x = np.arange(0, len(data_a), 1)
+
+		plt.figure()
+
+		bar_a = plt.bar(x - 0.25, data_a, color='r', width=0.5)
+		bar_b = plt.bar(x + 0.25, data_b, color='b', width=0.5)
+
+		if self.y_scale != None: #Needed because matplotlib does not like setting the base for linear plots
+			plt.yscale(self.y_scale, base=self.y_base)
+
+		plt.title(self.title)
+		plt.xlabel(self.x_label)
+		plt.ylabel(self.y_label)
+
+		if self.x_tick_labels != None:
+			plt.xticks(x, labels=self.x_tick_labels, rotation=self.x_tick_label_rotation, ha=self.x_tick_label_horizontal_alignment)
+
+		plt.legend([data_a_name, data_b_name])
+
+		plt.show()
+
+	def comparision_box_plot(self, data_a, data_b, data_a_name, data_b_name, show_fliers):
+		"""
+		Generates a box plot comparing two data sets.
+		"""
+		assert len(data_a) == len(data_b)
+
+		a_positions = []
+		b_positions = []
+		x_ticks = []
+		for i in range(0, len(data_a) + len(data_b)):
+			x_ticks.append(i)
+		for i in range(0, len(data_a)):
+			a_positions.append(i*2)
+		for i in range(0, len(data_b)):
+			b_positions.append(i*2+1)
+
+		plt.figure()
+
+		box_a = plt.boxplot(data_a, patch_artist=True, positions=a_positions, showfliers=show_fliers)
+		for box in box_a['boxes']:
+			box.set(color='red', linewidth=1)
+			box.set(facecolor='white')
+		for flier in box_a['fliers']:
+			flier.set(markersize=0.5, alpha=0.5)
+
+		box_b = plt.boxplot(data_b, patch_artist=True, positions=b_positions, showfliers=show_fliers)
+		for box in box_b['boxes']:
+			box.set(color='blue', linewidth=1)
+			box.set(facecolor='white')
+		for flier in box_b['fliers']:
+			flier.set(markersize=0.5, alpha=0.5)
+
+		if self.y_scale != None: #Needed because matplotlib does not like setting the base for linear plots
+			plt.yscale(self.y_scale, base=self.y_base)
+
+		plt.title(self.title)
+		plt.xlabel(self.x_label)
+		plt.ylabel(self.y_label)
+		if self.x_tick_labels != None:
+			#Generate x tick labels
+			new_x_tick_labels = []
+			for label in self.x_tick_labels:
+				new_x_tick_labels.append(label + " (" + data_a_name + ")")
+				new_x_tick_labels.append(label + " (" + data_b_name + ")")
+
+			plt.xticks(x_ticks, labels=new_x_tick_labels, rotation=self.x_tick_label_rotation, ha=self.x_tick_label_horizontal_alignment)
+
+		plt.legend([box_a['boxes'][0], box_b['boxes'][0]], [data_a_name, data_b_name])
+
+		plt.show()
+
+#Example plots using Plot_Generator
+if __name__ == '__main__':
+	#Create some fake data
+	bar_plot_data_a = []
+	bar_plot_data_b = []
+	for i in range(1, 11):
+		bar_plot_data_a.append((i+1))
+		bar_plot_data_b.append((i+1))
+
+	box_plot_data_a = []
+	box_plot_data_b = []
+	for i in range(0, 10):
+		box_plot_data_a.append([])
+		box_plot_data_b.append([])
+		for j in range(0, 10):
+			box_plot_data_a[i].append((i+1) * (j+1))
+			box_plot_data_b[i].append((i+1) / (j+1))
+
+	#Set up the plot generator
+	pg = Plot_Generator()
+	pg.set_text_options(45, 'right', 0, 'center')
+	pg.set_text('Title', 'X', 'Y', ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], None)
+
+	#Plot the bar plot data
+	pg.comparision_bar_plot(bar_plot_data_a, bar_plot_data_b, 'A', 'B')
+
+	#Plot the box plot data
+	#Note the x tick labels. 
+	#It will automatically generate this using the previously set x tick labels and given data_a_name and data_b_name values
+	pg.comparision_box_plot(box_plot_data_a, box_plot_data_b, 'A', 'B', False)
