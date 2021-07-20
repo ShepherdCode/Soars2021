ca048763a0df2dd186db4008b75ae2c2adc4961c
JasonRafeMiller <jmill02@shepherd.edu>
2021-07-20 07:03:35 -0400

    Add scripts for recent notebooks.

---
+++ Scripts/MLP_GenCode_109.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MLP GenCode 
+# MLP_GenCode_108 with one change.  
+# Now use GenCode 38 instead of 26.  
+# accuracy: 99.95%, AUC: 99.95%
+
+# In[1]:
+
+
+import time
+def show_time():
+    t = time.time()
+    print(time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(t)))
+show_time()
+
+
+# In[2]:
+
+
+PC_TRAINS=8000
+NC_TRAINS=8000
+PC_TESTS=8000
+NC_TESTS=8000   # Wen et al 2019 used 8000 and 2000 of each class
+PC_LENS=(200,99000)
+NC_LENS=(200,99000)    # Wen et al 2019 used 250-3500 for lncRNA only
+MAX_K = 3
+INPUT_SHAPE=(None,84)  # 4^3 + 4^2 + 4^1
+NEURONS=128
+DROP_RATE=0.25
+EPOCHS=100 # 25
+SPLITS=5
+FOLDS=5   # make this 5 for serious testing
+
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+
+from keras.models import Sequential
+from keras.layers import Dense,Embedding,Dropout
+from keras.layers import Flatten,TimeDistributed
+from keras.losses import BinaryCrossentropy
+from keras.callbacks import ModelCheckpoint
+
+
+# In[4]:
+
+
+import sys
+IN_COLAB = False
+try:
+    from google.colab import drive
+    IN_COLAB = True
+except:
+    pass
+if IN_COLAB:
+    print("On Google CoLab, mount cloud-local file, get our code from GitHub.")
+    PATH='/content/drive/'
+    #drive.mount(PATH,force_remount=True)  # hardly ever need this
+    drive.mount(PATH)    # Google will require login credentials
+    DATAPATH=PATH+'My Drive/data/'  # must end in "/"
+    import requests
+    r = requests.get('https://raw.githubusercontent.com/ShepherdCode/Soars2021/master/SimTools/GenCodeTools.py')
+    with open('GenCodeTools.py', 'w') as f:
+        f.write(r.text)  
+    from GenCodeTools import GenCodeLoader
+    r = requests.get('https://raw.githubusercontent.com/ShepherdCode/Soars2021/master/SimTools/KmerTools.py')
+    with open('KmerTools.py', 'w') as f:
+        f.write(r.text)  
+    from KmerTools import KmerTools
+else:
+        print("CoLab not working. On my PC, use relative paths.")
+        DATAPATH='data/'  # must end in "/"
+        sys.path.append("..") # append parent dir in order to use sibling dirs
+        from SimTools.GenCodeTools import GenCodeLoader
+        from SimTools.KmerTools import KmerTools
+MODELPATH="BestModel"  # saved on cloud instance and lost after logout
+#MODELPATH=DATAPATH+MODELPATH  # saved on Google Drive but requires login
+
+
+# ## Data Load
+# Restrict mRNA to those transcripts with a recognized ORF.
+
+# In[5]:
+
+
+PC_FILENAME='gencode.v26.pc_transcripts.fa.gz'
+NC_FILENAME='gencode.v26.lncRNA_transcripts.fa.gz'
+PC_FILENAME='gencode.v38.pc_transcripts.fa.gz'
+NC_FILENAME='gencode.v38.lncRNA_transcripts.fa.gz'
+PC_FULLPATH=DATAPATH+PC_FILENAME
+NC_FULLPATH=DATAPATH+NC_FILENAME
+
+
+# In[6]:
+
+
+loader=GenCodeLoader()
+loader.set_label(1)
+loader.set_check_utr(False)
+pcdf=loader.load_file(PC_FULLPATH)
+print("PC seqs loaded:",len(pcdf))
+loader.set_label(0)
+loader.set_check_utr(False)
+ncdf=loader.load_file(NC_FULLPATH)
+print("NC seqs loaded:",len(ncdf))
+show_time()
+
+
+# ## Data Prep
+
+# In[7]:
+
+
+def dataframe_length_filter(df,low_high):
+    (low,high)=low_high
+    # The pandas query language is strange, 
+    # but this is MUCH faster than loop & drop.
+    return df[ (df['seqlen']>=low) & (df['seqlen']<=high) ]
+def dataframe_shuffle(df):
+    # The ignore_index option is new in Pandas 1.3. 
+    # The default (False) replicates the old behavior: shuffle the index too.
+    # The new option seems more logical th
+    # After shuffling, df.iloc[0] has index == 0.
+    # return df.sample(frac=1,ignore_index=True)
+    return df.sample(frac=1)  # Use this till CoLab upgrades Pandas
+def dataframe_extract_sequence(df):
+    return df['sequence'].tolist()
+
+pc_all = dataframe_extract_sequence(
+    #dataframe_shuffle(
+    dataframe_length_filter(pcdf,PC_LENS))#)
+nc_all = dataframe_extract_sequence(
+    #dataframe_shuffle(
+    dataframe_length_filter(ncdf,NC_LENS))#)
+
+#pc_all=['CAAAA','CCCCC','AAAAA','AAACC','CCCAA','CAAAA','CCCCC','AACAA','AAACC','CCCAA']
+#nc_all=['GGGGG','TTTTT','GGGTT','GGGTG','TTGTG','GGGGG','TTTTT','GGTTT','GGGTG','TTGTG']
+
+show_time()
+print("PC seqs pass filter:",len(pc_all))
+print("NC seqs pass filter:",len(nc_all))
+# Garbage collection to reduce RAM footprint
+pcdf=None
+ncdf=None
+
+
+# In[8]:
+
+
+# Any portion of a shuffled list is a random selection
+pc_train=pc_all[:PC_TRAINS] 
+nc_train=nc_all[:NC_TRAINS]
+pc_test=pc_all[PC_TRAINS:PC_TRAINS+PC_TESTS] 
+nc_test=nc_all[NC_TRAINS:NC_TRAINS+PC_TESTS]
+print("PC train, NC train:",len(pc_train),len(nc_train))
+print("PC test, NC test:",len(pc_test),len(nc_test))
+# Garbage collection
+pc_all=None
+nc_all=None
+
+
+# In[9]:
+
+
+def prepare_x_and_y(seqs1,seqs0):
+    len1=len(seqs1)
+    len0=len(seqs0)
+    total=len1+len0
+    L1=np.ones(len1,dtype=np.int8)
+    L0=np.zeros(len0,dtype=np.int8)
+    S1 = np.asarray(seqs1)
+    S0 = np.asarray(seqs0)
+    all_labels = np.concatenate((L1,L0))
+    all_seqs = np.concatenate((S1,S0))  
+    return all_seqs,all_labels  # use this to test unshuffled
+    # bug in next line?
+    X,y = shuffle(all_seqs,all_labels) # sklearn.utils.shuffle 
+    #Doesn't fix it
+    #X = shuffle(all_seqs,random_state=3) # sklearn.utils.shuffle 
+    #y = shuffle(all_labels,random_state=3) # sklearn.utils.shuffle 
+    return X,y
+Xseq,y=prepare_x_and_y(pc_train,nc_train)
+print(Xseq[:3])
+print(y[:3])
+# Tests:
+show_time()
+
+
+# In[10]:
+
+
+def seqs_to_kmer_freqs(seqs,max_K):
+    tool = KmerTools()  # from SimTools
+    empty = tool.make_dict_upto_K(max_K)
+    collection = []
+    for seq in seqs:
+        counts = empty
+        # Last param should be True when using Harvester.
+        counts = tool.update_count_one_K(counts,max_K,seq,True)
+        # Given counts for K=3, Harvester fills in counts for K=1,2.
+        counts = tool.harvest_counts_from_K(counts,max_K)
+        fdict = tool.count_to_frequency(counts,max_K)
+        freqs = list(fdict.values())
+        collection.append(freqs)
+    return np.asarray(collection)
+Xfrq=seqs_to_kmer_freqs(Xseq,MAX_K)
+show_time()
+
+
+# ## Neural network
+
+# In[11]:
+
+
+def make_DNN():
+    dt=np.float32
+    print("make_DNN")
+    print("input shape:",INPUT_SHAPE)
+    dnn = Sequential()
+    dnn.add(Dense(NEURONS,activation="sigmoid",dtype=dt))  # relu doesn't work as well
+    dnn.add(Dropout(DROP_RATE))
+    dnn.add(Dense(NEURONS,activation="sigmoid",dtype=dt)) 
+    dnn.add(Dropout(DROP_RATE))
+    dnn.add(Dense(1,activation="sigmoid",dtype=dt))   
+    dnn.compile(optimizer='adam',    # adadelta doesn't work as well
+                loss=BinaryCrossentropy(from_logits=False),
+                metrics=['accuracy'])   # add to default metrics=loss
+    dnn.build(input_shape=INPUT_SHAPE) 
+    return dnn
+model = make_DNN()
+print(model.summary())
+
+
+# In[12]:
+
+
+def do_cross_validation(X,y):
+    cv_scores = []
+    fold=0
+    mycallbacks = [ModelCheckpoint(
+        filepath=MODELPATH, save_best_only=True, 
+        monitor='val_accuracy', mode='max')]   
+    splitter = KFold(n_splits=SPLITS,shuffle=True)
+    for train_index,valid_index in splitter.split(X):
+        if fold < FOLDS:
+            fold += 1
+            X_train=X[train_index] # inputs for training
+            y_train=y[train_index] # labels for training
+            X_valid=X[valid_index] # inputs for validation
+            y_valid=y[valid_index] # labels for validation
+            print("MODEL")
+            # Call constructor on each CV. Else, continually improves the same model.
+            model = model = make_DNN()
+            print("FIT")  # model.fit() implements learning
+            start_time=time.time()
+            history=model.fit(X_train, y_train, 
+                    epochs=EPOCHS, 
+                    verbose=1,  # ascii art while learning
+                    callbacks=mycallbacks,   # called at end of each epoch
+                    validation_data=(X_valid,y_valid))
+            end_time=time.time()
+            elapsed_time=(end_time-start_time)                        
+            print("Fold %d, %d epochs, %d sec"%(fold,EPOCHS,elapsed_time))
+            # print(history.history.keys())  # all these keys will be shown in figure
+            pd.DataFrame(history.history).plot(figsize=(8,5))
+            plt.grid(True)
+            plt.gca().set_ylim(0,1) # any losses > 1 will be off the scale
+            plt.show()
+
+
+# In[13]:
+
+
+do_cross_validation(Xfrq,y)
+
+
+# In[14]:
+
+
+from keras.models import load_model
+print(pc_train[0])
+Xseq,y=prepare_x_and_y(pc_train,nc_train)
+print(Xseq[0])
+Xfrq=seqs_to_kmer_freqs(Xseq,MAX_K)
+print(Xfrq[0])
+X=Xfrq
+best_model=load_model(MODELPATH)
+scores = best_model.evaluate(X, y, verbose=0)
+print("The best model parameters were saved during cross-validation.")
+print("Best was defined as maximum validation accuracy at end of any epoch.")
+print("Now re-load the best model and test it on previously unseen data.")
+print("Test on",len(pc_test),"PC seqs")
+print("Test on",len(nc_test),"NC seqs")
+print("%s: %.2f%%" % (best_model.metrics_names[1], scores[1]*100))
+
+
+# In[15]:
+
+
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+ns_probs = [0 for _ in range(len(y))]
+bm_probs = best_model.predict(X)
+print("predictions.shape",bm_probs.shape)
+print("first prediction",bm_probs[0])
+ns_auc = roc_auc_score(y, ns_probs)
+bm_auc = roc_auc_score(y, bm_probs)
+ns_fpr, ns_tpr, _ = roc_curve(y, ns_probs)
+bm_fpr, bm_tpr, _ = roc_curve(y, bm_probs)
+plt.plot(ns_fpr, ns_tpr, linestyle='--', label='Guess, auc=%.4f'%ns_auc)
+plt.plot(bm_fpr, bm_tpr, marker='.', label='Model, auc=%.4f'%bm_auc)
+plt.title('ROC')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend()
+plt.show()
+print("%s: %.2f%%" %('AUC',bm_auc*100.0))
+
+
+# In[15]:
+
+
+
+
